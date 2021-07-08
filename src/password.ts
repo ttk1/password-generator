b05a1fdc620d931305204b3d7eac6d2b1eb61371
tama <tama@ttk1.net>
2021-07-08 21:33:46 +0900

    乱数を Math.random -> window.crypto.getRandomValues に変更

--- src/password.ts
+++ src/password.ts
@@ -1,11 +1,9 @@
-function getRandomChar(passwordChars) {
-  return passwordChars[Math.floor(Math.random() * passwordChars.length)];
-}
-
 export function generatePassword(passwordLength: number, passwordChars: string[]): string {
   const password: string[] = [];
+  const random = new Uint32Array(passwordLength);
+  window.crypto.getRandomValues(random);
   for (let i = 0; i < passwordLength; i++) {
-    password.push(getRandomChar(passwordChars));
+    password.push(passwordChars[random[i] % passwordChars.length]);
   }
   return password.join('');
 }
